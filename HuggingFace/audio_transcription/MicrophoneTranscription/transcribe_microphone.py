30349e595362c9c75e20d8681cb61099c2823399
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-10-10 21:15:34 -0700

    ASR update 2 - log file and cache expansion

--- HuggingFace/audio_transcription/MicrophoneTranscription/transcribe_microphone.py
+++ HuggingFace/audio_transcription/MicrophoneTranscription/transcribe_microphone.py
@@ -15,7 +15,7 @@ class RealTimeASR:
     """
     This class demonstrates how to perform real-time ASR using the pipeline method of the Transformers library.
     """
-    def __init__(self):
+    def __init__(self, maxlen=300):
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
         self.asr_pipeline = pipeline(
             "automatic-speech-recognition",
@@ -24,7 +24,7 @@ class RealTimeASR:
             device=self.device,
             return_timestamps=True
         )
-        self.transcription_cache = deque(maxlen=100)
+        self.transcription_cache = deque(maxlen=maxlen)
         self.sliding_window = np.array([])
 
     def initialize_audio(self):
@@ -35,12 +35,16 @@ class RealTimeASR:
                                   input=True,
                                   frames_per_buffer=1024)
 
-    def capture_and_transcribe(self):
+    def capture_and_transcribe(self, log_file=None):
         """
         Continuously captures audio from the microphone, concatenates it to a sliding window, and transcribes the audio
         using the ASR pipeline. If the sliding window is longer than 30 seconds, the pipeline is run on the first 30 seconds
         of audio and the sliding window is shifted by 5 seconds. If there is a transcription in the cache, it is printed to
-        stdout.
+        stdout and written to the log file.
+
+        Args:
+            log_file (str): The path to the log file to write transcriptions to. If None, transcriptions will not be written
+            to a log file.
 
         Returns:
             None
@@ -58,22 +62,33 @@ class RealTimeASR:
                 self.transcription_cache.append(transcription["text"])
                 self.sliding_window = self.sliding_window[16000 * 5:]
 
-            # If there is a transcription in the cache, print it to stdout
+            # If there is a transcription in the cache, print it to stdout and write it to the log file
             if len(self.transcription_cache) > 0:
-                print(self.transcription_cache.pop(), file=sys.stdout, flush=True)
+                transcription = self.transcription_cache.pop()
+                print(transcription, file=sys.stdout, flush=True)
+                if log_file is not None:
+                    with open(log_file, "a") as f:
+                        f.write(transcription + "\n")
 
-    def close_stream(self):
+    def close_stream(self, log_file=None):
         self.stream.stop_stream()
         self.stream.close()
         self.p.terminate()
 
+        # Write the final transcription to the log file
+        if log_file is not None:
+            with open(log_file, "a") as f:
+                for transcription in self.transcription_cache:
+                    f.write(transcription + "\n")
 
+# Example usage                 
 if __name__ == "__main__":
-    asr_app = RealTimeASR()
+    asr_app = RealTimeASR(maxlen=300)
     asr_app.initialize_audio()
+    log_file = "transcription_log.txt"
     try:
-        asr_app.capture_and_transcribe()
+        asr_app.capture_and_transcribe(log_file=log_file)
     except KeyboardInterrupt:
         print("Stopping transcription.")
     finally:
-        asr_app.close_stream()
+        asr_app.close_stream(log_file=log_file)
