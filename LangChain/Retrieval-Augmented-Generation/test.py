9034a6bf7c6096803d473f66dcfcaea482cec288
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-10-11 18:12:34 -0700

    Add test module
    	new file:   LangChain/Retrieval-Augmented-Generation/test.py

---
+++ LangChain/Retrieval-Augmented-Generation/test.py
@@ -0,0 +1,32 @@
+import unittest
+from unittest.mock import patch, MagicMock
+from qa_local_docs import PDFProcessor
+
+# Assumes that 'data/' directory contains PDFs
+class TestPDFProcessor(unittest.TestCase):
+    def setUp(self):
+        self.pdf_processor = PDFProcessor()
+
+    def test_load_pdfs_from_directory(self):
+        # Test that the method returns a non-empty list
+        result = self.pdf_processor.load_pdfs_from_directory()
+        self.assertTrue(isinstance(result, list))
+        self.assertTrue(len(result) > 0)
+
+    def test_perform_similarity_search(self):
+        # Test that the method returns a non-empty list
+        texts = self.pdf_processor.load_pdfs_from_directory()
+        result = self.pdf_processor.perform_similarity_search(texts, "test")
+        self.assertTrue(isinstance(result, list))
+        self.assertTrue(len(result) > 0)
+
+    @patch('qa_local_docs.ChatOpenAI')
+    @patch('qa_local_docs.Chroma')
+    @patch('qa_local_docs.UniversalSentenceEncoder')
+    def test_answer_question(self, mock_embeddings, mock_vectorstore, mock_llm):
+        # Test that the method returns a string
+        mock_result = MagicMock()
+        mock_result.__getitem__.return_value = {"result": "test answer"}
+        mock_llm.return_value = mock_result
+        result = self.pdf_processor.answer_question("test question")
+        self.assertTrue(isinstance(result, str))
