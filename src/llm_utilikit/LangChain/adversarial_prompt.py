5cf6d5ffc6f69113d63160beee8480047b247b42
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-11-28 17:15:35 -0800

    Update adversarial_prompt.py
    Signed-off-by: Daemon <109057945+Daethyra@users.noreply.github.com>

--- src/llm_utilikit/LangChain/adversarial_prompt.py
+++ src/llm_utilikit/LangChain/adversarial_prompt.py
@@ -1,47 +1,58 @@
-# adversarial_prompt.py
-import json
 import logging
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.schema.runnable import RunnableLambda, RunnableBranch
+from langchain.schema.output_parser import StrOutputParser
+from langchain.chat_models import ChatOpenAI
 
-class AdversarialPrompt:
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+class AdversarialPromptChain:
     def __init__(self):
         self.llm = OpenAI()
-        logging.basicConfig(level=logging.INFO)
 
-    def generate_initial_prompt(self, query):
-        return f"Provide a detailed analysis of the following query, considering different perspectives: '{query}'"
+    def initial_prompt_step(self, query):
+        prompt = f"Provide a detailed analysis of the following query, considering different perspectives: '{query}'"
+        return self.llm.run(prompt)
 
-    def generate_confidence_check_prompt(self, response):
-        return f"Assess the reliability and depth of the answer '{response}'. Identify potential biases or limitations."
+    def confidence_check_step(self, response):
+        prompt = f"Assess the reliability and depth of the answer '{response}'. Identify potential biases or limitations."
+        return self.llm.run(prompt)
 
-    def generate_reasoning_validation_prompt(self, response):
-        return f"Deconstruct the logic of the answer '{response}'. Validate the reasoning and scrutinize for any inaccuracies or oversights."
+    def reasoning_validation_step(self, response):
+        prompt = f"Deconstruct the logic of the answer '{response}'. Validate the reasoning and scrutinize for any inaccuracies or oversights."
+        return self.llm.run(prompt)
 
-    def generate_perspective_sharing_prompt(self):
-        return "If there's uncertainty in your response, elucidate the reasoning behind this uncertainty. Discuss alternative interpretations or viewpoints."
+    def perspective_sharing_step(self):
+        prompt = "If there's uncertainty in your response, elucidate the reasoning behind this uncertainty. Discuss alternative interpretations or viewpoints."
+        return self.llm.run(prompt)
 
-    def execute_prompts(self, query):
-        try:
-            logging.info(f"Executing prompts for query: {query}")
-            initial_response = self.llm.run(self.generate_initial_prompt(query))
-            confidence_response = self.llm.run(self.generate_confidence_check_prompt(initial_response))
-            if self.is_response_confident(confidence_response):
-                final_response = self.llm.run(self.generate_reasoning_validation_prompt(initial_response))
-            else:
-                final_response = self.llm.run(self.generate_perspective_sharing_prompt())
-            logging.info("Finished executing prompts.")
-        except Exception as e:
-            logging.error(f"Error during prompt execution: {e}")
-            return "An error occurred while processing the query."
+    def create_chain(self):
+        # Define each step as RunnableLambdas
+        initial_prompt_runnable = RunnableLambda(self.initial_prompt_step)
+        confidence_check_runnable = RunnableLambda(self.confidence_check_step)
+        reasoning_validation_runnable = RunnableLambda(self.reasoning_validation_step)
+        perspective_sharing_runnable = RunnableLambda(self.perspective_sharing_step)
 
-        tracer = Trace()
-        return tracer.postprocess(final_response)
+        # Define routing logic using RunnableBranch
+        routing_branch = RunnableBranch(
+            (lambda response: 'high confidence' in response.lower(), reasoning_validation_runnable),
+            (lambda response: True, perspective_sharing_runnable)  # Default branch
+        )
 
-    def is_response_confident(self, response):
-        return 'high confidence' in response.lower()
+        # Create the full chain
+        full_chain = initial_prompt_runnable | confidence_check_runnable | routing_branch
+
+        return full_chain
+
+# Expose the chain for import
+adversarial_prompt_chain = AdversarialPromptChain().create_chain()
 
 if __name__ == "__main__":
-    query = input("Please enter your query: ")
-    ap = AdversarialPrompt()
-    result = ap.execute_prompts(query)
-    print(result)
+    try:
+        query = input("Please enter your query: ")
+        result = adversarial_prompt_chain.invoke(query)
+        print(result)
+    except Exception as e:
+        logging.error(f"An error occurred during execution: {e}")
