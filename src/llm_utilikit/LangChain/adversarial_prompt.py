6d8c46f2a0c97558b9c2d4ce631d2ca7bf1f7206
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-11-28 17:32:21 -0800

    Update adversarial_prompt.py
    Signed-off-by: Daemon <109057945+Daethyra@users.noreply.github.com>

--- src/llm_utilikit/LangChain/adversarial_prompt.py
+++ src/llm_utilikit/LangChain/adversarial_prompt.py
@@ -1,9 +1,21 @@
 import logging
+import os
+from dotenv import load_dotenv
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.schema.runnable import RunnableLambda, RunnableBranch
 from langchain.schema.output_parser import StrOutputParser
 from langchain.chat_models import ChatOpenAI
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the variables
+LANGCHAIN_TRACING_V2 = os.getenv('LANGCHAIN_TRACING_V2')
+LANGCHAIN_ENDPOINT = os.getenv('LANGCHAIN_ENDPOINT')
+LANGCHAIN_API_KEY = os.getenv('LANGCHAIN_API_KEY')
+LANGCHAIN_PROJECT = os.getenv('LANGCHAIN_PROJECT', 'Adversarial-Prompt')  # Default to "Adversarial-Prompt" if not set
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -11,21 +23,26 @@ logging.basicConfig(level=logging.INFO)
 class AdversarialPromptChain:
     def __init__(self):
         self.llm = OpenAI()
+        logging.info("AdversarialPromptChain initialized with OpenAI model.")
 
     def initial_prompt_step(self, query):
         prompt = f"Provide a detailed analysis of the following query, considering different perspectives: '{query}'"
+        logging.info(f"Generating initial prompt for query: {query}")
         return self.llm.run(prompt)
 
     def confidence_check_step(self, response):
         prompt = f"Assess the reliability and depth of the answer '{response}'. Identify potential biases or limitations."
+        logging.info("Generating confidence check prompt.")
         return self.llm.run(prompt)
 
     def reasoning_validation_step(self, response):
         prompt = f"Deconstruct the logic of the answer '{response}'. Validate the reasoning and scrutinize for any inaccuracies or oversights."
+        logging.info("Generating reasoning validation prompt.")
         return self.llm.run(prompt)
 
-    def perspective_sharing_step(self):
-        prompt = "If there's uncertainty in your response, elucidate the reasoning behind this uncertainty. Discuss alternative interpretations or viewpoints."
+    def perspective_sharing_step(self, response):
+        prompt = f"If there's uncertainty in the response '{response}', elucidate the reasoning behind this uncertainty. Discuss alternative interpretations or viewpoints."
+        logging.info("Generating perspective sharing prompt.")
         return self.llm.run(prompt)
 
     def create_chain(self):
@@ -36,6 +53,16 @@ class AdversarialPromptChain:
         perspective_sharing_runnable = RunnableLambda(self.perspective_sharing_step)
 
         # Define routing logic using RunnableBranch
+        def route_logic(response):
+            logging.info("Evaluating response for routing decision.")
+            if confidence_check_runnable(response):
+                logging.info("Routing to reasoning validation step.")
+                return reasoning_validation_runnable
+            else:
+                logging.info("Routing to perspective sharing step.")
+                return perspective_sharing_runnable(response)
+
+
         routing_branch = RunnableBranch(
             (lambda response: 'high confidence' in response.lower(), reasoning_validation_runnable),
             (lambda response: True, perspective_sharing_runnable)  # Default branch
