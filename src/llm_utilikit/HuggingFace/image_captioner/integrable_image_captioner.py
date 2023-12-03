3d3d91ae1b0b9486ce629d321e471f9b2914c805
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-12-03 14:28:58 -0800

    Fix integrable_image_captioner.py #61
    chore(image_captioner): improve CSV handling and error logging
    
    Enhanced the method for saving image captions to a CSV file, allowing for flexibility in specifying the file write mode. Additionally, improved error handling for potential issues during CSV writing. If a file object is provided, it now takes precedence over the file name. Also included more descriptive error logging for unexpected errors that occur during the process.

--- src/llm_utilikit/HuggingFace/image_captioner/integrable_image_captioner.py
+++ src/llm_utilikit/HuggingFace/image_captioner/integrable_image_captioner.py
@@ -2,9 +2,7 @@ import os
 import glob
 import logging
 import csv
-import json
 from datetime import datetime
-from dotenv import load_dotenv
 import asyncio
 import torch
 from PIL import Image, UnidentifiedImageError
@@ -106,26 +104,30 @@ class ImageCaptioner:
             logger.error(f"Failed to generate caption: {e}")
             return None
 
-    def save_to_csv(self, image_name: str, caption: str, file_name: str = None, csvfile=None):
+    def save_to_csv(self, image_name: str, caption: str, file_name: str = None, csvfile=None, mode='a'):
         """
-        Saves the image name and the generated caption to a CSV file, supporting both file name and file object inputs.
-        
-        This method now includes enhanced error handling to manage potential issues when writing to the CSV file.
-        
+        Saves the image name and the generated caption to a CSV file. This method supports writing to a CSV file using either a file name or a file object. If a file object is provided, it takes precedence over the file name.
+
+        Enhanced error handling is included to manage potential issues when writing to the CSV file. The method also allows for specifying the file write mode, adding flexibility in how the CSV file is handled (e.g., append or write).
+
         Args:
             image_name (str): The name of the image file.
             caption (str): The generated caption.
-            file_name (str, optional): The name of the CSV file. Defaults to a timestamp-based name.
-            csvfile (file object, optional): The CSV file to write to. Takes precedence over file_name if provided.
+            file_name (str, optional): The name of the CSV file. If not provided, a timestamp-based name is used.
+            csvfile (file object, optional): The CSV file to write to. If provided, this takes precedence over file_name.
+            mode (str, optional): The file mode for writing to the CSV (e.g., 'a' for append, 'w' for write). Defaults to 'a'.
         """
-        if file_name is None:
-            file_name = f"captions_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv"
+        if csvfile is None and file_name is not None:
+            csvfile = open(file_name, mode, newline='')
+        
         try:
-            with open(file_name, 'a', newline='') as csvfile:
-                writer = csv.writer(csvfile)
-                writer.writerow([image_name, caption])
+            writer = csv.writer(csvfile)
+            writer.writerow([image_name, caption])
         except Exception as e:
             logger.error(f"Failed to write to CSV file: {e}")
+        finally:
+            if csvfile and file_name is not None:
+                csvfile.close()
 
 async def main() -> None:
     """
@@ -157,13 +159,13 @@ async def main() -> None:
             # Check if the image was successfully loaded
             if raw_image:
                 # Generate a caption, conditionally or unconditionally, based on the configuration
-                caption = await captioner.generate_caption(raw_image, config['ENDING_CAPTION']) if use_conditional_caption else await captioner.generate_caption(raw_image)
+                caption = await captioner.generate_caption(raw_image, ending_caption) if use_conditional_caption else await captioner.generate_caption(raw_image)
     
                 # Save the image file name and its generated caption to a CSV file
                 captioner.save_to_csv(os.path.basename(image_file), caption)
         except Exception as e:
             # Log any errors that occur during the caption generation or CSV writing process
-            logger.error(f"An unexpected error occurred: {e}")
+            logger.error(f"An unexpected error occurred with {image_file}: {e}")
 
 # Entry point for the script execution
 if __name__ == "__main__":
